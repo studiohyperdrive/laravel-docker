49296eaddaffb9ee520eeb8074201c5eb00306ef
Jasper De Smet <jasp.desmet@gmail.com>
2018-11-08 15:26:59 +0100

    Update links on homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,28 +66,16 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Gastles Docker
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://studiohyperdrive.be" target="_blank">Studio Hyperdrive</a>
+                    <a href="https://www.dockerleren.be" target="_blank">dockerleren.be</a>
+                    <a href="https://docs.docker.com" target="_blank">Docker docs</a>
+                    <a href="https://github.com/studiohyperdrive/laravel-docker" target="_blank">Github</a>
                 </div>
             </div>
         </div>
